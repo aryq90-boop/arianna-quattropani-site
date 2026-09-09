8f458cfd16cab7f4391d5cdd08f589bea41c9455
aryq90-boop <aryq90@gmail.com>
2026-09-09 20:01:14 +0200

    Add files via upload
    Update homepage slideshow

--- assets/home.js
+++ assets/home.js
@@ -1,11 +1,10 @@
 const slides = [...document.querySelectorAll('.home-slide')];
 let current = 0;
+
 function show(next) {
   slides[current].classList.remove('is-active');
   current = (next + slides.length) % slides.length;
   slides[current].classList.add('is-active');
 }
-document.querySelector('.hero-hit-left').addEventListener('click', () => show(current - 1));
-document.querySelector('.hero-hit-right').addEventListener('click', () => show(current + 1));
-let timer = setInterval(() => show(current + 1), 6500);
-document.querySelector('.home-hero').addEventListener('pointerenter', () => clearInterval(timer));
+
+setInterval(() => show(current + 1), 6000);
